66ae8cadd50f7e776f59d9a8ba66987c21a89433
wisehead <alex.chenhui@gmail.com>
2022-03-10 14:01:07 +0800

    8001 maven ok

---
+++ springCloud/workspace/spring-cloud-demo2/micro-service-cloud-provider-dept-8001/src/main/java/net/biancheng/c/MicroServiceCloudProviderDept8001Application.java
@@ -0,0 +1,13 @@
+package net.biancheng.c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceCloudProviderDept8001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceCloudProviderDept8001Application.class, args);
+    }
+
+}
