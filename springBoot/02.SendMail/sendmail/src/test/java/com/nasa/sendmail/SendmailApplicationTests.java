f63555d1ece9525cf4866f0a97c20e32471beb34
wisehead <alex.chenhui@gmail.com>
2022-03-14 13:59:09 +0800

    send mail

--- springBoot/02.SendMail/sendmail/src/test/java/com/nasa/sendmail/SendmailApplicationTests.java
+++ springBoot/02.SendMail/sendmail/src/test/java/com/nasa/sendmail/SendmailApplicationTests.java
@@ -1,10 +1,15 @@
 package com.nasa.sendmail;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
+/*
 @SpringBootTest
 class SendmailApplicationTests {
 
@@ -13,11 +18,12 @@ class SendmailApplicationTests {
 	}
 
 }
+*/
 
-/*
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SendmailApplication.class)
-class SendmailApplicationTests {
+//@SpringApplicationConfiguration(classes = SendmailApplication.class)
+@SpringBootTest
+public class SendmailApplicationTests {
 
 	@Autowired
 	private JavaMailSender mailSender;
@@ -25,7 +31,7 @@ class SendmailApplicationTests {
 	@Test
 	public void sendSimpleMail() throws Exception {
 		SimpleMailMessage message = new SimpleMailMessage();
-		message.setFrom("dyc87112@qq.com");
+		message.setFrom("chenhui_bupt@126.com");
 		message.setTo("chenhui@elensdata.com");
 		message.setSubject("主题：简单邮件");
 		message.setText("测试邮件内容");
@@ -34,4 +40,3 @@ class SendmailApplicationTests {
 	}
 
 }
-*/
