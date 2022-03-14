a080852a94c5f73b8439b93ca8bb67201c3fea35
wisehead <alex.chenhui@gmail.com>
2022-03-14 14:45:46 +0800

    send mail with attachment

--- springBoot/02.SendMail/sendmail/src/test/java/com/nasa/sendmail/SendmailApplicationTests.java
+++ springBoot/02.SendMail/sendmail/src/test/java/com/nasa/sendmail/SendmailApplicationTests.java
@@ -5,9 +5,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 /*
 @SpringBootTest
@@ -37,6 +42,27 @@ public class SendmailApplicationTests {
 		message.setText("测试邮件内容");
 
 		mailSender.send(message);
+	}
+
+	@Test
+	public void sendAttachmentsMail() throws Exception {
+
+		MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+		helper.setFrom("chenhui_bupt@126.com");
+		helper.setTo("chenhui@elensdata.com");
+		helper.setSubject("主题：有附件");
+		helper.setText("有附件的邮件");
+
+		FileSystemResource file = new FileSystemResource(new File("weixin.jpg"));
+		//helper.addAttachment("附件-1.jpg", file);
+		//helper.addAttachment("附件-2.jpg", file);
+		helper.addAttachment("fujian1.jpg", file);
+		//helper.addAttachment("fujian2.jpg", file);
+
+		mailSender.send(mimeMessage);
+
 	}
 
 }
