b80a0f77789e90bdbd1cdf8ceeaf57a9b4559506
wisehead <alex.chenhui@gmail.com>
2022-03-14 16:07:38 +0800

    freemaker

--- springBoot/02.SendMail/sendmail/src/test/java/com/nasa/sendmail/SendmailApplicationTests.java
+++ springBoot/02.SendMail/sendmail/src/test/java/com/nasa/sendmail/SendmailApplicationTests.java
@@ -1,6 +1,10 @@
 package com.nasa.sendmail;
 
 //import org.junit.jupiter.api.Test;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +15,12 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Date;
 
 /*
 @SpringBootTest
@@ -82,5 +90,32 @@ public class SendmailApplicationTests {
 		mailSender.send(mimeMessage);
 
 	}
-
+	//    Freemarker 作邮件模板
+	@Test
+	public void sendFreemarkerMail() throws MessagingException, IOException, TemplateException {
+		MimeMessage mimeMessage = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+		helper.setSubject("这是一封测试邮件");
+		helper.setFrom("chenhui_bupt@126.com");
+		helper.setTo("chenhui@elensdata.com");
+		//helper.setCc("37xxxxx37@qq.com");
+		//helper.setBcc("14xxxxx098@qq.com");
+		helper.setSentDate(new Date());
+		//构建 Freemarker 的基本配置
+		Configuration configuration = new Configuration(Configuration.VERSION_2_3_0);
+		// 配置模板位置，启动类的位置JhjmailApplication
+		ClassLoader loader = SendmailApplication.class.getClassLoader();
+		configuration.setClassLoaderForTemplateLoading(loader, "templates");
+		//加载模板
+		Template template = configuration.getTemplate("mail.ftl");
+		User user = new User();
+		user.setUsername("男人不如狗");
+		user.setNum(1);
+		user.setSalary(99999.0);
+		StringWriter out = new StringWriter();
+		//模板渲染，渲染的结果将被保存到 out 中 ，将out 中的 html 字符串发送即可
+		template.process(user, out);
+		helper.setText(out.toString(),true);
+		mailSender.send(mimeMessage);
+	}
 }
