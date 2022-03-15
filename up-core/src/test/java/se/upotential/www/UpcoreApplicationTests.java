9eb72182fe44c6c9ba3c212fa034d3402dfe79b1
wisehead <alex.chenhui@gmail.com>
2022-03-15 10:28:16 +0800

    send mail ok

--- up-core/src/test/java/se/upotential/www/UpcoreApplicationTests.java
+++ up-core/src/test/java/se/upotential/www/UpcoreApplicationTests.java
@@ -1,7 +1,19 @@
 package se.upotential.www;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.util.Date;
 
 @SpringBootTest
 class MybatisApplicationTests {
@@ -10,4 +22,113 @@ class MybatisApplicationTests {
     void contextLoads() {
     }
 
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Test
+    public void sendSimpleMail() throws Exception {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("chenhui_bupt@126.com");
+        message.setTo("chenhui@elensdata.com");
+        message.setSubject("主题：简单邮件");
+        message.setText("测试邮件内容");
+
+        mailSender.send(message);
+    }
+
+    @Test
+    public void sendAttachmentsMail() throws Exception {
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+        helper.setFrom("chenhui_bupt@126.com");
+        helper.setTo("chenhui@elensdata.com");
+        helper.setSubject("主题：有附件");
+        helper.setText("有附件的邮件");
+
+        FileSystemResource file = new FileSystemResource(new File("weixin.jpg"));
+        //helper.addAttachment("附件-1.jpg", file);
+        //helper.addAttachment("附件-2.jpg", file);
+        helper.addAttachment("fujian1.jpg", file);
+        //helper.addAttachment("fujian2.jpg", file);
+        mailSender.send(mimeMessage);
+    }
+
+    @Test
+    public void sendInlineMail() throws Exception {
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+        helper.setFrom("chenhui_bupt@126.com");
+        helper.setTo("chenhui@elensdata.com");
+        helper.setSubject("主题：嵌入静态资源");
+        helper.setText("<html><body><img src=\"cid:weixin\" ></body></html>", true);
+
+        FileSystemResource file = new FileSystemResource(new File("weixin.jpg"));
+        helper.addInline("weixin", file);
+
+        mailSender.send(mimeMessage);
+
+    }
+    /*
+    //    Freemarker 作邮件模板
+    @Test
+    public void sendFreemarkerMail() throws MessagingException, IOException, TemplateException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+        helper.setSubject("这是一封测试邮件");
+        helper.setFrom("chenhui_bupt@126.com");
+        helper.setTo("chenhui@elensdata.com");
+        //helper.setCc("37xxxxx37@qq.com");
+        //helper.setBcc("14xxxxx098@qq.com");
+        helper.setSentDate(new Date());
+        //构建 Freemarker 的基本配置
+        Configuration configuration = new Configuration(Configuration.VERSION_2_3_0);
+        // 配置模板位置，启动类的位置JhjmailApplication
+        ClassLoader loader = SendmailApplication.class.getClassLoader();
+        configuration.setClassLoaderForTemplateLoading(loader, "templates");
+        //加载模板
+        Template template = configuration.getTemplate("mail.ftl");
+        User user = new User();
+        user.setUsername("男人不如狗");
+        user.setNum(1);
+        user.setSalary(99999.0);
+        StringWriter out = new StringWriter();
+        //模板渲染，渲染的结果将被保存到 out 中 ，将out 中的 html 字符串发送即可
+        template.process(user, out);
+        helper.setText(out.toString(),true);
+        mailSender.send(mimeMessage);
+    }
+    */
+
+    @Autowired
+    TemplateEngine templateEngine;
+
+    @Test
+    public void sendThymeleafMail() throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = null;
+        try {
+            helper = new MimeMessageHelper(mimeMessage, true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        helper.setSubject("这是一封测试邮件");
+        helper.setFrom("chenhui_bupt@126.com");
+        helper.setTo("chenhui@elensdata.com");
+        //helper.setFrom("1510161612@qq.com");
+        //helper.setTo("25xxxxx755@qq.com");
+        helper.setCc("alex.chenhui@gmail.com");
+        //helper.setBcc("98@qq.com");
+        helper.setSentDate(new Date());
+        Context context = new Context();
+        context.setVariable("username", "javaboy");
+        context.setVariable("num","000001");
+        context.setVariable("salary", "99999");
+        String process = templateEngine.process("mail.html", context);
+        helper.setText(process,true);
+        mailSender.send(mimeMessage);
+    }
 }
